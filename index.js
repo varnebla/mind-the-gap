a41c24aa8fce763c7a1fc3df67046f877a75ef62
Victor Arnedo Blanco <victor@Air-de-Victor.lan>
2019-11-14 19:14:47 +0100

    add description in index.js

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-// countMissingCarriages("BCE") // 2
+/*
+You must implement a function that receives the sequence of carriages as a string of carriage
+letters and returns the number of missing carriages. The train carriages are initially lettered
+from 'A' onwards with no gaps. Several carriages may be missing, but the remaining carriages are
+in their original sequence order. The highest-lettered carriage can never be missing (because that
+is where the engine is).
+
+The number of carriages in a train can vary from 1 to 26 (i.e. 'A' to 'Z').
+
+Example:
+countMissingCarriages("ABC") //should return 0 because all carriages are present.
+countMissingCarriages("ABD") //should return 1 because carriage C is missing.
+countMissingCarriages("BCF") //should return 3 because carriages A, D, and E are missing.
+*/
+
 function countMissingCarriages(train) {
 
   //WRITE YOUR CODE HERE!!🤘
