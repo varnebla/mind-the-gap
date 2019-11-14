899f29a1b456f415948fbd36e32ce008caa68224
Timoteo Ladmann <teoladmann@gmail.com>
2019-11-14 20:30:03 +0100

    complete

--- index.js
+++ index.js
@@ -8,15 +8,25 @@ is where the engine is).
 The number of carriages in a train can vary from 1 to 26 (i.e. 'A' to 'Z').
 
 Example:
-countMissingCarriages("ABC") //should return 0 
-countMissingCarriages("ABD") //should return 1 
-countMissingCarriages("BCF") //should return 3 
+countMissingCarriages("ABC") //should return 0
+countMissingCarriages("ABD") //should return 1
+countMissingCarriages("BCF") //should return 3
 */
 
 function countMissingCarriages(train) {
-
-  //WRITE YOUR CODE HERE!!🤘
-
+  let trainArr = train.split('');
+  let counter = 0;
+  let abc =
+  'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+  let j = 0;
+  for (let i = 0; i < trainArr.length; i++) {
+    for (; j < abc.length; j++) {
+      if (trainArr[i] === abc[j]) break;
+      else counter++;
+    }
+    j++;
+  }
+  return counter;
 }
 
 module.exports = countMissingCarriages;
